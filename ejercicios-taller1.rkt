5cf61a17c0e1d91cb67e65ed37cf6a4cae094503
JhonierM14 <144240957+JhonierM14@users.noreply.github.com>
2025-02-19 07:15:12 -0500

    Create ejercicios-taller1.rkt
    Estructura

---
+++ ejercicios-taller1.rkt
@@ -0,0 +1,41 @@
+|#
+
+Jhonier Mendez Bravo 202372226
+
+#|
+
+
+(define invert
+  ( lambda (L)
+    ()
+
+  )
+)
+
+(define down
+  ( lambda (L)
+    ()
+
+  )
+)
+
+(define list-set
+  ( lambda (L n x)
+    ()
+
+  )
+)
+
+(define filter-in
+  ( lambda (P L)
+    ()
+
+  )
+)
+
+(define list-index
+  ( lambda (P L)
+    ()
+
+  )
+)
