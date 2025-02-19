19467879fc520ed9c024e6dd05a844ada907d468
JhonierM14 <144240957+JhonierM14@users.noreply.github.com>
2025-02-19 07:36:49 -0500

    Update ejercicios-taller1.rkt
    .

--- ejercicios-taller1.rkt
+++ ejercicios-taller1.rkt
@@ -39,3 +39,99 @@ Jhonier Mendez Bravo 202372226
 
   )
 )
+
+(define swapper
+  ( lambda (E1 E2 L)
+    ()
+
+  )
+)
+
+(define cartesian-product
+  ( lambda (L1 L2)
+    ()
+
+  )
+)
+
+(define mapping
+  ( lambda (F L1 L2)
+    ()
+
+  )
+)
+
+(define inversions
+  ( lambda (L)
+    ()
+
+  )
+)
+
+(define up
+  ( lambda (L)
+    ()
+
+  )
+)
+
+(define insert
+  ( lambda (L)
+    ()
+
+  )
+)
+
+(define zip
+  ( lambda (L)
+    ()
+
+  )
+)
+
+(define filter-acum
+  ( lambda (a b F acum filter)
+    ()
+
+  )
+)
+
+(define (operate lrators lrands)
+  ( 
+
+  )
+)
+
+(define path
+  ( lambda (n BST)
+    ()
+
+  )
+)
+
+
+
+(define (count-odd-and-even arbol)
+  (
+
+  )
+)
+
+(define (prod-scalar-matriz mat vec)
+  (
+
+  )
+)
+
+
+(define (Operar-binarias operacionB) 
+  (
+
+  )
+)
+
+(define (pascal N) 
+  (
+
+  )
+)
