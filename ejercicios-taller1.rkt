081a954edb20f2e6c4f04123bacb8cf707880f48
Juanpa0108 <jrobayomaestre@gmail.com>
2025-02-20 10:40:33 -0500

    Ejercicio 3 y 4

--- ejercicios-taller1.rkt
+++ ejercicios-taller1.rkt
@@ -1,6 +1,7 @@
 #|
 
 Jhonier Mendez Bravo 202372226
+Juan Pablo Robayo Maestre 202156743
 
 |#
 
@@ -34,19 +35,61 @@ Jhonier Mendez Bravo 202372226
   )
 )
 
-(define filter-in
-  ( lambda (P L)
-    ()
+#|
+filter-in :
+ Proposito:
+ P x L -> L' : Procedimiento que retorna una lista con los elementos de L que cumplen el predicado P.
+ <lista> ::= ()
+         ::= (<valor> <lista>)
+ <predicado> ::= procedimiento que devuelve #t o #f para un <valor>
+ Ejemplo:
+(filter-in even? '(1 2 3 4 5 6))  => '(2 4 6)
+|#
 
-  )
-)
+(define filter-in
+  (lambda (predicado lista)
+    (if (null? lista)  
+        '()
+        (if (predicado (car lista))  
+            (cons (car lista) (filter-in predicado (cdr lista)))  
+            (filter-in predicado (cdr lista))))))
+
+#|
+list-index :
+Proposito:
+P x L -> N / #f : Procedimiento que retorna el índice del primer elemento de L que satisface P.
+Si ningún elemento satisface P, retorna #f.
+<lista> ::= ()
+        ::= (<valor> <lista>)
+<predicado> ::= procedimiento que devuelve #t o #f para un <valor>
+<indice> ::= número natural que representa la posición en la lista
+Ejemplo:
+(list-index even? '(1 3 5 6 7))  => 3
+(list-index even? '(1 3 5 7 9))  => #f
+
+
+helper :
+ Proposito:
+ L x N -> N / #f : Procedimiento auxiliar que recorre la lista con un índice acumulador.
+ Retorna el índice del primer elemento que satisface P, o #f si no hay coincidencias.
+ <lista> ::= ()
+         ::= (<valor> <lista>)
+<indice> ::= número natural que representa la posición actual en la lista
+ Ejemplo:
+ (helper '(6 7 8) 2)  => 2
+|#
 
 (define list-index
-  ( lambda (P L)
-    ()
+  (lambda (predicado lista)   
+    (define helper
+      (lambda (lista index)
+        (if (null? lista)  
+            #f
+            (if (predicado (car lista))  
+                index
+                (helper (cdr lista) (+ index 1)))))) (helper lista 0))  
 
-  )
-)
+    ) 
 
 (define swapper
   ( lambda (E1 E2 L)
