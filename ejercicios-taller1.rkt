f7d3620b58daae7dac66dfbbaa1bf4d5011f841e
JhonierM14 <144240957+JhonierM14@users.noreply.github.com>
2025-02-24 22:29:46 -0500

    Update ejercicios-taller1.rkt

--- ejercicios-taller1.rkt
+++ ejercicios-taller1.rkt
@@ -1,8 +1,13 @@
-#|
+;;-----------------------------------------
+;; Jhonier Mendez Bravo 202372226
+;; David Santiago Guerrero Delgado 202324594
+;; Juan Pablo Robayo Maestre 202156743
+;;-----------------------------------------
 
-Jhonier Mendez Bravo 202372226
-
-|#
+;; <List> ::= ()
+;;        ::= (<Pair> <list>)
+;; <Pair> ::= ()
+;;        ::= (<Scheme-Value> <<Scheme-Value>)
 
 
 (define invert
@@ -21,19 +26,33 @@ Jhonier Mendez Bravo 202372226
 )
 
 (define down
-  ( lambda (L)
-    ()
-
+  (lambda (L)
+    (cond
+    [(null? L) empty]
+    [else (cons (list (car L)) (down (cdr L)))]
+    )
   )
 )
+
+;; Pruebas
+(down '(1 2 3))
+(down '((una) (buena) (idea)))
+(down '(un (objeto (mas)) complicado))
 
 (define list-set
-  ( lambda (L n x)
-    ()
-
+  (lambda (L n x)
+    (cond
+      [(> (+ n 1) (length L)) "Error: no se pudo reemplazar el valor x en la posicion n"]
+      [(null? L) empty]
+      [(= n 0) (cons x (cdr L))]
+      [else (cons (car L) (list-set (cdr L) (- n 1) x))]
+    )
   )
 )
 
+;; Pruebas
+(list-set '(a b c d) 2 '(1 2))
+(list-set '(a b c d) 3 '(1 5 10))
 (define filter-in
   ( lambda (P L)
     ()
@@ -41,12 +60,42 @@ Jhonier Mendez Bravo 202372226
   )
 )
 
-(define list-index
-  ( lambda (P L)
-    ()
+#|
+list-index :
+Proposito:
+P x L -> N / #f : Procedimiento que retorna el índice del primer elemento de L que satisface P.
+Si ningún elemento satisface P, retorna #f.
+<lista> ::= ()
+        ::= (<valor> <lista>)
+<predicado> ::= procedimiento que devuelve #t o #f para un <valor>
+<indice> ::= número natural que representa la posición en la lista
+Ejemplo:
+(list-index even? '(1 3 5 6 7))  => 3
+(list-index even? '(1 3 5 7 9))  => #f
 
-  )
-)
+
+helper :
+ Proposito:
+ L x N -> N / #f : Procedimiento auxiliar que recorre la lista con un índice acumulador.
+ Retorna el índice del primer elemento que satisface P, o #f si no hay coincidencias.
+ <lista> ::= ()
+         ::= (<valor> <lista>)
+<indice> ::= número natural que representa la posición actual en la lista
+ Ejemplo:
+ (helper '(6 7 8) 2)  => 2
+|#
+
+(define list-index
+  (lambda (predicado lista)   
+    (define helper
+      (lambda (lista index)
+        (if (null? lista)  
+            #f
+            (if (predicado (car lista))  
+                index
+                (helper (cdr lista) (+ index 1)))))) (helper lista 0))  
+
+) 
 
 (define swapper
   ( lambda (E1 E2 L)
@@ -98,11 +147,20 @@ Jhonier Mendez Bravo 202372226
 )
 
 (define filter-acum
-  ( lambda (a b F acum filter)
-    ()
-
+  (lambda (a b F acum filter)
+    (cond
+      [(> a b) acum]
+      [(filter a) (filter-acum (+ a 1) b F (F acum a) filter)]
+      [else (filter-acum (+ a 1) b F acum filter)]
+    )
   )
 )
+
+;; Pruebas 
+(filter-acum 1 10 + 0 odd?)
+(filter-acum 1 10 + 0 even?)
+(filter-acum 1 10 - 0 odd?)
+(filter-acum 1 10 - 0 even?)
 
 (define (operate lrators lrands)
   ( 
